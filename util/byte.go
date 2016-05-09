6ce4547558a5677ce21ce627193e70f333822983
Eduardo Acha <eduardoacha@gmail.com>
2016-05-08 21:28:02 -0300

    Added new byte util functions

--- util/byte.go
+++ util/byte.go
@@ -7,3 +7,17 @@ func CopySliceInto(source, target []byte, pos int) int {
 
 	return pos + len(source)
 }
+
+func ByteIndexOf(slice []byte, b byte, fromIndex int) int {
+	if len(slice) <= fromIndex {
+		return -1
+	}
+
+	for i := fromIndex; i < len(slice); i++ {
+		if slice[i] == b {
+			return i
+		}
+	}
+
+	return -1
+}
